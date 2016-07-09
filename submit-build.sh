5f56a7c95f651e160d514e2372f19ca8279b1273
Petr Šabata <contyk@redhat.com>
2016-07-09 18:17:16 +0200

    Make submit-build.sh a bit more user-friendly
    By adding some line breaks.
    
    Signed-off-by: Petr Šabata <contyk@redhat.com>

--- submit-build.sh
+++ submit-build.sh
@@ -2,4 +2,6 @@
 echo "Submmiting a build of modules/testmodule, #020ea37251df5019fde9e7899d2f7d7a987dfbf5"
 echo "Using https://localhost:5000/rida/module-builds/"
 echo "NOTE: You need to be a Fedora packager for this to work"
+echo
 curl --cert ~/.fedora.cert -k -H "Content-Type: text/json" --data @submit-build.json https://localhost:5000/rida/module-builds/
+echo
