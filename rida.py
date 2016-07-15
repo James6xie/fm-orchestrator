2eee1c8ed66602958dc9462222858842c336ba1a
Ralph Bean <rbean@redhat.com>
2016-07-15 11:23:32 -0400

    Descriptions of the states.

--- rida.py
+++ rida.py
@@ -123,7 +123,7 @@ def submit_build():
         build = rida.database.ComponentBuild(module_id=module.id, package=pkgname, format="rpms")
         db.session.add(build)
     module.modulemd = mmd.dumps()
-    module.state = rida.database.BUILD_STATES["init"]
+    module.state = rida.database.BUILD_STATES["wait"]
     db.session.add(module)
     db.session.commit()
     # Publish to whatever bus we're configured to connect to.
