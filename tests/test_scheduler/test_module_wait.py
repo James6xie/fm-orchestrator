62654f871fed7d595874b5c61a175699f275af59
Matt Prahl <mprahl@redhat.com>
2016-11-16 09:24:08 -0500

    Fixes bug where the build tag was not returned

--- tests/test_scheduler/test_module_wait.py
+++ tests/test_scheduler/test_module_wait.py
@@ -40,6 +40,7 @@ class TestModuleWait(unittest.TestCase):
         builder = mock.Mock()
         builder.get_disttag_srpm.return_value = 'some srpm disttag'
         builder.build.return_value = 1234, 1, "", None
+        builder.module_build_tag = {'name': 'some-tag-build'}
         KojiModuleBuilder.return_value = builder
         mocked_module_build = mock.Mock()
         mocked_module_build.json.return_value = {
