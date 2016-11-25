27ae1eafd40b68f7bc048b2e93bd04af406a12d1
Filip Valder <fvalder@redhat.com>
2016-11-25 17:48:12 +0100

    initial setup.py, see FIXMEs

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+with open('test-requirements.txt') as f:
+    test_requirements = f.readlines()
+
+setup(name='module-build-service',
+      description='The Module Build Service for Modularity',
+      version='1.0.0',
+      classifiers=[
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Build Tools"
+      ],
+      keywords='module build service fedora modularity koji mock rpm',
+      author='FIXME',
+      author_email='FIXME',
+      url='https://pagure.io/fm-orchestrator/',
+      license='GPLv2+',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requirements,
+      tests_require=test_requirements,
+      entry_points={
+          'console_scripts': ['module_build_service_daemon = module_build_service.scheduler.main:main']
+      },
+      )
