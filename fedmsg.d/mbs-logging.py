d13c7991ed2fc2f2652a82ba4a4941a0a257912b
Ralph Bean <rbean@redhat.com>
2016-12-12 15:09:58 -0500

    Cleanup in preparation for package review.
    I'm taking on #235.  This is all stuff that came up while doing a
    cursory package review myself.
    
    - The source files all state that they are MIT, but the specfile and
      repo claimed to be GPL.  I switched everything to MIT (even though I
      favor GPL.. the source was MIT first before it was incorrectly
      labelled GPL).
    - There are various bugs and lint issues in the specfile fixed here.
    - Notably, the systemd service file is missing from the 1.0.0 tarball on
      PyPI, so I'll need to do a new release after this.

--- fedmsg.d/mbs-logging.py
+++ fedmsg.d/mbs-logging.py
@@ -1,7 +1,3 @@
-# Setup fedmsg logging.
-# See the following for constraints on this format https://bit.ly/Xn1WDn
-bare_format = "[%(asctime)s][%(name)10s %(levelname)7s] %(message)s"
-
 config = dict(
     logging=dict(
         loggers=dict(
--- module_build_service/__init__.py
+++ module_build_service/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-
 # Copyright (c) 2016  Red Hat, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
