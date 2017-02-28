2312efce4526d6a86a8bdf4fb96868b9b2a4680c
Jakub Kadlčík <frostyx@email.cz>
2017-02-28 16:10:00 +0100

    Not use PDC for copr builder

--- module_build_service/scheduler/handlers/modules.py
+++ module_build_service/scheduler/handlers/modules.py
@@ -154,7 +154,7 @@ def wait(config, session, msg):
     tag = None
     dependencies = []
 
-    if conf.system == "mock":
+    if conf.system in ["mock", "copr"]:
         # In case of mock, we do not try to get anything from pdc,
         # just generate our own koji_tag to identify the module in messages.
         tag = '-'.join(['module', module_info['name'],
