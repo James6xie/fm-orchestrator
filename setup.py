a459b91fde500a014161ee602605461575253e4a
Ralph Bean <rbean@redhat.com>
2017-03-03 10:06:06 -0500

    1.3.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('test-requirements.txt') as f:
 
 setup(name='module-build-service',
       description='The Module Build Service for Modularity',
-      version='1.3.2',
+      version='1.3.3',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Software Development :: Build Tools"
