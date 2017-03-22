f743aefe5acde388c02d6424352aac7f0f570bc7
Ralph Bean <rbean@redhat.com>
2017-03-22 13:09:29 -0400

    More bootstrap scripts.

--- bootstrap/deactivate-in-pdc.py
+++ bootstrap/deactivate-in-pdc.py
@@ -11,6 +11,10 @@ if os.path.basename(__file__) in (servername, token, variant_uid,):
 print("Connecting to PDC server %r with token %r" % (servername, token))
 pdc = pdc_client.PDCClient(servername, token=token)
 
-print("Submitting DELETE.")
-del pdc['unreleasedvariants'][variant_uid]
+print("Querying for %r to see if it is active" % variant_uid)
+obj = pdc['unreleasedvariants'][variant_uid]()
+assert obj['active'] == True, obj['active']
+
+print("Submitting PATCH to deactivate.")
+pdc['unreleasedvariants'][variant_uid] += {'variant_uid': variant_uid, 'active': False}
 print("Done.")
