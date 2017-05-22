6bea509833bd89711eda95e9b52fd06e5788e955
Jakub Kadlčík <frostyx@email.cz>
2017-05-22 18:08:51 +0200

    Set concurrent treshold for copr same as for mock

--- module_build_service/utils.py
+++ module_build_service/utils.py
@@ -93,6 +93,9 @@ def at_concurrent_component_threshold(config, session):
     if conf.system == "mock":
         return False
 
+    if conf.system == "copr":
+        return False
+
     import koji  # Placed here to avoid py2/py3 conflicts...
 
     if config.num_consecutive_builds and config.num_consecutive_builds <= \
