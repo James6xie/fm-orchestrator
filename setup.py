74993a4df2f14079f4775a803b6198a324640a45
mprahl <mprahl@redhat.com>
2017-11-03 16:30:17 -0400

    Bump to version v1.4.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('test-requirements.txt') as f:
 
 setup(name='module-build-service',
       description='The Module Build Service for Modularity',
-      version='1.4.2',
+      version='1.4.3',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Software Development :: Build Tools"
