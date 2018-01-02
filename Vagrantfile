bc4a612b3850d8a9a54e4101757ecbb4c22fc64d
mprahl <mprahl@redhat.com>
2018-01-02 17:05:02 -0500

    Stop using site_packages when running tox

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,6 @@ $script = <<SCRIPT
         fedmsg-relay \
         fedpkg \
         gcc \
-        gcc \
         gcc-c++ \
         git \
         koji \
--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+from os import path
+
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.readlines()
 
-with open('test-requirements.txt') as f:
-    test_requirements = f.readlines()
+def read_requirements(filename):
+    specifiers = []
+    dep_links = []
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.startswith('-r') or line.strip() == '':
+                continue
+            if line.startswith('git+'):
+                dep_links.append(line.strip())
+            else:
+                specifiers.append(line.strip())
+    return specifiers, dep_links
+
+
+setup_py_path = path.dirname(path.realpath(__file__))
+install_requires, deps_links = read_requirements(path.join(setup_py_path, 'requirements.txt'))
+tests_require, _ = read_requirements(path.join(setup_py_path, 'test-requirements.txt'))
 
 setup(name='module-build-service',
       description='The Module Build Service for Modularity',
@@ -21,8 +36,9 @@ setup(name='module-build-service',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=requirements,
-      tests_require=test_requirements,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      dependency_links=deps_links,
       entry_points={
           'console_scripts': ['mbs-upgradedb = module_build_service.manage:upgradedb',
                               'mbs-frontend = module_build_service.manage:run',
