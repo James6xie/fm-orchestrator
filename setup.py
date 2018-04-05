b502c18369802e512b904c277f238b067458f750
Jan Kaluza <jkaluza@redhat.com>
2018-04-05 14:22:26 +0200

    Version 2.0.0.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require, _ = read_requirements(path.join(setup_py_path, 'test-requirements
 
 setup(name='module-build-service',
       description='The Module Build Service for Modularity',
-      version='1.7.0',
+      version='2.0.0',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Software Development :: Build Tools"
