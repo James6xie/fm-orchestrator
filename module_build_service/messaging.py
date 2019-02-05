b4d3c512197bfe49445b5fc986150c40b78b3a01
Mike Bonnet <mikeb@redhat.com>
2019-02-05 10:41:11 -0800

    don't assign the exception to a variable to make flake8 happy

--- module_build_service/messaging.py
+++ module_build_service/messaging.py
@@ -295,7 +295,7 @@ def _in_memory_publish(topic, msg, conf, service):
         work_queue_put(wrapped_msg)
     except ValueError as e:
         log.warning("No MBSConsumer found.  Shutting down?  %r" % e)
-    except AttributeError as e:
+    except AttributeError:
         # In the event that `moksha.hub._hub` hasn't yet been initialized, we
         # need to store messages on the side until it becomes available.
         # As a last-ditch effort, try to hang initial messages in the config.
