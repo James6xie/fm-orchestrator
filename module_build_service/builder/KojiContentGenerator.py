73d71e11b81a7c16d7319eb53af739a93a64274c
Jan Kaluza <jkaluza@redhat.com>
2019-02-12 15:21:04 +0100

    Revert "KojiContentGenerator: Convert MMD to UTF-8"
    This reverts commit ae79b711d87a7f460fad256ba7ac81bd1c285b55.
    
    This breaks MBS with python3 and fixes only single occurence of this
    issue. We want to fix this in libmodulemd or find a better way how
    to fix this to work on both python2 and python3.
    
    We have libmodulemd PR open to address this issue on libmodulemd
    level: https://github.com/fedora-modularity/libmodulemd/issues/184.

--- module_build_service/builder/KojiContentGenerator.py
+++ module_build_service/builder/KojiContentGenerator.py
@@ -710,7 +710,7 @@ class KojiContentGenerator(object):
         # Fill in the list of built RPMs.
         mmd = self._fill_in_rpms_list(mmd, arch)
 
-        return text_type(mmd.dumps(), "utf-8")
+        return text_type(mmd.dumps())
 
     def _download_source_modulemd(self, mmd, output_path):
         """
